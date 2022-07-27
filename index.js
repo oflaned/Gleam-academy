d588130b918a2eea94b96073472df135a0db206d
oflaned <misha122000@icloud.com>
2022-07-27 16:00:25 +0300

    Books request

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/health', (req, res)=> {
 app.get('/books/:bookid?', (req, res) => {
     let data = JSON.parse(fs.readFileSync(process.env.booksData))
     if(!req.params.bookid) { res.send(data) }
-    else if(isNaN(req.params.bookid) || req.params.bookid < 0 ) res.sendStatus(400)
+    else if(!Number.isInteger(Number(req.params.bookid)) || req.params.bookid.includes('-')) res.sendStatus(400)// указать минимальную для запуска ноду - 16 версии
     else if(Number(req.params.bookid) < data.length){
         res.send(data[Number(req.params.bookid)]) 
     } else res.sendStatus(404)
